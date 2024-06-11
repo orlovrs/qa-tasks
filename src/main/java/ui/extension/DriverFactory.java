40479f79c47f09619b4fe247ca7be387528abe33
orlovrs <pomah_89@inbox.ru>
2024-06-11 10:20:43 +0200

    add browser

--- src/main/java/ui/extension/DriverFactory.java
+++ src/main/java/ui/extension/DriverFactory.java
@@ -2,6 +2,7 @@ package ui.extension;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -36,7 +37,9 @@ public class DriverFactory {
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--headless=new");
+            options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
             return new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+//            return new ChromeDriver(options);
         } catch (Exception ignored) { }
         return null;
     }
